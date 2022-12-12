0d986bf2ca874f82d10bb432f903627c80f831ec
charlieJenson <charlesjenson.caingal@student.dmmmsu.edu.ph>
2022-12-12 22:43:16 +0800

    Added delete and edit functions to shool page

--- DocTS/api/public/index.php
+++ DocTS/api/public/index.php
@@ -516,5 +516,68 @@ $app->post('/fetchSchools', function (Request $request, Response $response, arra
     return $response;
 });
 
+$app->post('/updateSchool', function (Request $request, Response $response, array $args) {
+    $data = json_decode($request->getBody());
+
+    $id = $data->id;
+    $school_name = $data->school_name;
+
+    //Database
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "dtsystem";
+    try {
+        $conn = new
+
+            PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+        // set the PDO error mode to exception
+        $conn->setAttribute(
+            PDO::ATTR_ERRMODE,
+
+            PDO::ERRMODE_EXCEPTION
+        );
+
+        $sql = "UPDATE school set school_name='" . $school_name . "' where id='" . $id . "'";
+
+        // use exec() because no results are returned
+        $conn->exec($sql);
+        $response->getBody()->write(json_encode(array("status" => "success", "data" => null)));
+    } catch (PDOException $e) {
+        $response->getBody()->write(json_encode(array(
+            "status" => "error",
+
+            "message" => $e->getMessage()
+        )));
+    }
+    $conn = null;
+
+    return $response;
+});
+
+$app->post('/deleteSchool', function (Request $request, Response $response, array $args) {
+
+    $data = json_decode($request->getBody());
+    $id = $data->id;
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "dtsystem";;
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "DELETE FROM school where id='" . $id . "'";
+    if ($conn->query($sql) === TRUE) {
+        $response->getBody()->write(json_encode(array("status" => "success", "data" => null)));
+    }
+    $conn->close();
+
+    return $response;
+});
 
 $app->run();
